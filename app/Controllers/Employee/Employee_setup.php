4eee33e90a5cc413944ad5728107bd881b54be55
aryuhub <awsadmin@aryuenterprises.com>
2023-12-18 12:14:31 +0530

    Push Single Module

--- app/Controllers/Employee/Employee_setup.php
+++ app/Controllers/Employee/Employee_setup.php
@@ -19,7 +19,7 @@ class Employee_setup extends BaseController
             $this->data['title'] = 'Dashboard';
             echo view(EMPLOYEE_PATH . '/pages/dashboard', $this->data);
         } else {
-            $this->data['title'] = 'Employee Aryuid';
+            $this->data['title'] = 'Employee Login Id';
             return view(EMPLOYEE_PATH . '/pages/login', $this->data);
         }
     }
